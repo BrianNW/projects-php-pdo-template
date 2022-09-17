6f28fc65c5263d81b8dc9d79aafa6294f2fb9be5
Brian <briantechdom@gmail.com>
2022-09-16 17:09:53 -0700

    Setup PDO fetch object statement

--- index.php
+++ index.php
@@ -15,8 +15,13 @@
     $stmt = $pdo->query('SELECT * FROM posts');
 
     // Check if row = statement, then fetch results
-    while($row = $stmt->fetch()) {
-        echo $row['title']. '<br>';
+    // while($row = $stmt->fetch()) {
+    //     echo $row['title']. '<br>';
+    // }
+
+    // Create same statement as above as an object
+    while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+        echo $row->title. '<br>';
     }
  
 ?>
